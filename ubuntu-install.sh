1cc29fbe9b82410a3cca59422767a1d60a6596e4
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-08-02 18:18:17 -0700

    ubuntu-install: extract to /root/go not /root/go/go

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -9,8 +9,7 @@ export PWD=$(pwd)
 export TMPDIR="/tmp/fiz3d-org-install"
 mkdir -p $TMPDIR && cd $TMPDIR
 wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
-mkdir -p /root/go
-tar -xzf ./go1.4.2.linux-amd64.tar.gz -C /root/go
+tar -xzf ./go1.4.2.linux-amd64.tar.gz -C /root
 
 # Cleanup and go back to the previous working directory.
 rm -rf $TMPDIR
