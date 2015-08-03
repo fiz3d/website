ec47e44c6d879afe37946b4386302f13dfea7878
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-08-02 18:24:46 -0700

    ubuntu-install: use lowercase for bash variable names

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -3,14 +3,14 @@
 sudo apt-get install git
 
 # Save current working directory.
-export pwd=$(pwd)
+pwd=$(pwd)
 
 # Download and Go and install it to /root/go
-export TMPDIR="/tmp/fiz3d-org-install"
-mkdir -p $TMPDIR && cd $TMPDIR
+tmpdir="/tmp/fiz3d-org-install"
+mkdir -p $tmpdir && cd $tmpdir
 wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
 tar -xzf ./go1.4.2.linux-amd64.tar.gz -C /root
 
 # Cleanup and go back to the previous working directory.
-rm -rf $TMPDIR
+rm -rf $tmpdir
 cd $pwd
