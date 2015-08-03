973610403c885d524412ab61e57c5a44659f1109
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-08-02 18:28:12 -0700

    ubuntu-install: properly provision machine

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git
+sudo apt-get install git make
 
 # Save current working directory.
 pwd=$(pwd)
@@ -14,3 +14,12 @@ tar -xzf ./go1.4.2.linux-amd64.tar.gz -C /root
 # Cleanup and go back to the previous working directory.
 rm -rf $tmpdir
 cd $pwd
+
+# Download repository.
+mkdir -p /root/godev/src/github.com/fiz3d
+cd /root/godev/src/github.com/fiz3d
+git clone https://github.com/fiz3d/website
+
+# Provision
+cd website
+make provision
